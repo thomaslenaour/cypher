7f91e675f01258b91f3bd42db342026226f8a7bc
Thomas Le Naour <lenaourthomas@gmail.com>
2023-05-31 21:10:30 +0200

    feat(authentication): add facebook provider

--- libs/front/libs/auth/environment.d.ts
+++ libs/front/libs/auth/environment.d.ts
@@ -5,6 +5,8 @@ declare global {
       NEXTAUTH_SECRET: string;
       GOOGLE_CLIENT_ID: string;
       GOOGLE_CLIENT_SECRET: string;
+      FACEBOOK_CLIENT_ID: string;
+      FACEBOOK_CLIENT_SECRET: string;
       JWT_SECRET: string;
     }
   }
--- libs/front/libs/auth/src/lib/authentication.ts
+++ libs/front/libs/auth/src/lib/authentication.ts
@@ -1,5 +1,6 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import FacebookProvider from 'next-auth/providers/facebook';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
 import jwt from 'jsonwebtoken';
@@ -20,6 +21,10 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
   ],
   session: {
