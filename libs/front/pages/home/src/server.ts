fb0419784f64f4e6c7e5eefaf8c8bb9f1a01e927
Thomas Le Naour <lenaourthomas@gmail.com>
2023-07-08 15:11:56 +0200

    fix(home): remove dynamic export

--- libs/front/pages/home/src/server.ts
+++ libs/front/pages/home/src/server.ts
@@ -1,4 +1,4 @@
 // Use this file to export React server components
 export { default as RootLayout } from './lib/layout';
-export { default as HomePage, dynamic } from './lib/page';
+export { default as HomePage } from './lib/page';
 export { default as HomeLoading } from './lib/loading';
