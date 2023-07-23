2ef68d47aa1921ac8ea5549a97c7d971c0675583
alexboisseau <alex.boisseau79@gmail.com>
2023-07-23 10:41:25 +0200

    feat: update the user profile module providers adding resolver, service, repository and adapter

--- libs/api/user-profile/src/lib/user-profile.module.ts
+++ libs/api/user-profile/src/lib/user-profile.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { UserProfileResolver } from './user-profile.resolver';
+import { UserProfileService } from './user-profile.service';
+import { UserProfileRepository } from './user-profile.repository';
+import { UserProfilePrismaAdapter } from './adapters/prisma-user-profile.adapter';
 
 @Module({
   controllers: [],
-  providers: [],
+  providers: [
+    UserProfileResolver,
+    UserProfileService,
+    {
+      provide: UserProfileRepository,
+      useClass: UserProfilePrismaAdapter,
+    },
+  ],
   exports: [],
 })
 export class UserProfileModule {}
