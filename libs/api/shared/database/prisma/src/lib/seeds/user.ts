769926fe1b0457e41ed7736d016eeaaface7e01a
alexboisseau <alex.boisseau79@gmail.com>
2023-07-23 10:27:19 +0200

    feat(seed): add seeds for the user profile model and add three elements to the user seed

--- libs/api/shared/database/prisma/src/lib/seeds/user.ts
+++ libs/api/shared/database/prisma/src/lib/seeds/user.ts
@@ -2,9 +2,27 @@ import { Prisma } from '@prisma/client';
 
 export const user: Prisma.UserCreateManyInput[] = [
   {
-    id: 'clhxmr6gr000008l2d26y2ix0',
-    email: 'user@cypherapp.co',
+    id: 'thoxmr6gr000008l2d26y2tho',
+    email: 'thomas@cypherapp.co',
     emailVerified: new Date('2023-05-21T16:27:06.737Z'),
-    name: 'John Doe',
+    name: 'Thomas Le Naour',
+  },
+  {
+    id: 'thexmr6gr000008l2d26y2the',
+    email: 'theo@cypherapp.co',
+    emailVerified: new Date('2023-05-21T16:27:06.737Z'),
+    name: 'Théo Delas',
+  },
+  {
+    id: 'sasxmr6gr000008l2d26y2sas',
+    email: 'sascha@cypherapp.co',
+    emailVerified: new Date('2023-05-21T16:27:06.737Z'),
+    name: 'Sascha Salles',
+  },
+  {
+    id: 'alexmr6gr000008l2d26y2ale',
+    email: 'alex@cypherapp.co',
+    emailVerified: new Date('2023-05-21T16:27:06.737Z'),
+    name: 'Alex Boisseau',
   },
 ];
