9d3155d948f84815042884c5dbedf0fc82001898
alexboisseau <alex.boisseau79@gmail.com>
2023-07-23 11:34:08 +0200

    refactor(user profile resolver): use ArgsType decoractor for the userProfile query

--- libs/api/user-profile/src/lib/user-profile.resolver.ts
+++ libs/api/user-profile/src/lib/user-profile.resolver.ts
@@ -1,17 +1,14 @@
 import { Args, Query, Resolver } from '@nestjs/graphql';
 import { UserProfileService } from './user-profile.service';
-import { UserProfileUniqueFields } from './types';
 import { UserProfileObjectType } from './user-profile.model';
+import { UserProfileArgs } from './args/user-profile.args';
 
 @Resolver()
 export class UserProfileResolver {
   constructor(private readonly userProfileService: UserProfileService) {}
 
   @Query(() => UserProfileObjectType)
-  async userProfile(
-    @Args('key', { type: () => String }) key: UserProfileUniqueFields,
-    @Args('value', { type: () => String }) value: string
-  ) {
+  async userProfile(@Args() { key, value }: UserProfileArgs) {
     return await this.userProfileService.getUserProfile(key, value);
   }
 }
