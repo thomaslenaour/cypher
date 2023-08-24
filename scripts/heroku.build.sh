ee95766989cc40f0034131214e00f80262bf6c7c
Thomas Le Naour <lenaourthomas@gmail.com>
2023-08-24 21:01:36 +0200

    fix: ci

--- scripts/heroku.build.sh
+++ scripts/heroku.build.sh
@@ -15,7 +15,7 @@ fi
   cd $PROJECT_ROOT
 
   if [ "$NX_APP_NAME" = "api" ]; then
-    nx run api:prisma:generate
+    nx run prisma:generate
     nx build api --prod
   else
     echo "Error: no build config for NX_APP_NAME value '$NX_APP_NAME'"
