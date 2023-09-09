4700b4bffca2a87085d94342ed6eeaf887961125
Thomas Le Naour <lenaourthomas@gmail.com>
2023-09-09 12:41:28 +0200

    ci: fix

--- libs/front/pages/room/src/lib/components/InsideRoom/MiddleArea/MiddleArea.tsx
+++ libs/front/pages/room/src/lib/components/InsideRoom/MiddleArea/MiddleArea.tsx
@@ -25,7 +25,7 @@ export function InsideRoomMiddleArea({
   authenticated,
   micPermissionEnabled,
 }: InsideRoomMiddleProps) {
-  console.log('change string');
+  console.log('change stringd');
   const currentParticipant = useLocalParticipant();
   const participants = useParticipants();
   const currentPublisher = useMemo(() => {
