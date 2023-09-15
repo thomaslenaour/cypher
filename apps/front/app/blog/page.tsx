81259e963155bb60a90a7e311f7790f5657185e4
Théo Delas <theo.delas@gmail.com>
2023-09-15 20:15:07 +0200

    feat: minimize blog container

--- apps/front/app/blog/page.tsx
+++ apps/front/app/blog/page.tsx
@@ -1,6 +1,6 @@
 import { Footer, Header } from '@cypher/front/components/common';
 import React from 'react';
-import { Box, Stack } from '@cypher/front/shared/ui';
+import { Box, Container, Stack } from '@cypher/front/shared/ui';
 import { LeftSide } from './components/LeftSide';
 import { RightSide } from './components/RightSide';
 import { SvgBackground } from '../../../../libs/front/pages/root/src/lib/components/SvgBackground';
@@ -8,13 +8,15 @@ export default function Home() {
   return (
     <>
       <Header />
-      <Box mx={{ xs: 3, sm: 8, md: 10, lg: 15 }}>
-        <SvgBackground />
-        <Stack direction="row" spacing={4} sx={{ mt: 10, minHeight: '80vh' }}>
-          <LeftSide />
-          <RightSide />
-        </Stack>
-      </Box>
+      <Container>
+        <Box>
+          <SvgBackground />
+          <Stack direction="row" spacing={4} sx={{ mt: 10, minHeight: '80vh' }}>
+            <LeftSide />
+            <RightSide />
+          </Stack>
+        </Box>
+      </Container>
       <Footer />
     </>
   );
