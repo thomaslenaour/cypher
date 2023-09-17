4d3534d69b09e800b684f1db4eb0061eff2ab7b0
Thomas Le Naour <lenaourthomas@gmail.com>
2023-09-17 22:33:08 +0200

    feat(header): add responsive header

--- libs/front/components/common/src/lib/Header/Header.tsx
+++ libs/front/components/common/src/lib/Header/Header.tsx
@@ -1,46 +1,10 @@
-import Link from 'next/link';
-
-import { Box, Container, Divider, Typography } from '@cypher/front/shared/ui';
-
 import { authOptions, getServerSession } from '@cypher/front/libs/auth/server';
-import { HeaderRightComponent } from './RightComponent';
-
-interface HeaderProps {
-  authenticated: boolean;
-}
+import { ClientHeader } from './ClientHeader/ClientHeader';
 
 export async function Header() {
   const session = await getServerSession(authOptions);
 
-  return <HeaderContent authenticated={!!session} />;
-}
-
-function HeaderContent({ authenticated }: HeaderProps) {
-  return (
-    <Container>
-      <Box
-        component="header"
-        sx={{
-          position: 'relative',
-          zIndex: 2,
-          display: 'flex',
-          justifyContent: 'space-between',
-          py: 2,
-          px: {
-            lg: '0 !important',
-          },
-        }}
-      >
-        <Link href="/">
-          <Typography level="h2" fontStyle="italic" textTransform="uppercase">
-            Cypher
-          </Typography>
-        </Link>
-        <HeaderRightComponent authenticated={authenticated} />
-      </Box>
-      <Divider />
-    </Container>
-  );
+  return <ClientHeader authenticated={!!session} />;
 }
 
 export default Header;
