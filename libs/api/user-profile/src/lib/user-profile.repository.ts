ec710b5e534d1d496d2f3de90b7764677d1cce30
alexboisseau <alex.boisseau79@gmail.com>
2023-09-18 17:02:50 +0200

    feat: create a new mutation to update user profile

--- libs/api/user-profile/src/lib/user-profile.repository.ts
+++ libs/api/user-profile/src/lib/user-profile.repository.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { UserProfileUniqueFields } from './types';
 import { UserProfileObjectType } from './user-profile.model';
+import { UpdateUserProfileInput } from './inputs/UpdateUserProfileInput';
 
 @Injectable()
 export abstract class UserProfileRepository {
   abstract getUserProfile(
     key: UserProfileUniqueFields,
     value: string
+  ): Promise<UserProfileObjectType | null>;
+
+  abstract updateUserProfile(
+    userProfileId: string,
+    input: UpdateUserProfileInput
   ): Promise<UserProfileObjectType | null>;
 }
