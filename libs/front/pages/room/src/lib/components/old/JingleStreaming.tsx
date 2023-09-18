eb036f44c140fdd6b82bf7f89f6903f74295d1c7
Thomas Le Naour <lenaourthomas@gmail.com>
2023-09-18 11:06:01 +0200

    chore: remove old inside room folder

--- libs/front/pages/room/src/lib/components/old/JingleStreaming.tsx
+++
@@ -1,9 +0,0 @@
-import { forwardRef } from 'react';
-
-export const JingleStreaming = forwardRef<HTMLAudioElement>((_, ref) => {
-  return (
-    <audio ref={ref} controls>
-      <source src="/audios/jingle.wav" type="audio/wav" />
-    </audio>
-  );
-});
