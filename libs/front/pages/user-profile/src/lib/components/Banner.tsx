7cec2e1aef96645a452b58724db6de0864990bea
alexboisseau <alex.boisseau79@gmail.com>
2023-09-21 15:49:40 +0200

    feat: handle profile and banner upload updating fiels in database

--- libs/front/pages/user-profile/src/lib/components/Banner.tsx
+++ libs/front/pages/user-profile/src/lib/components/Banner.tsx
@@ -1,32 +1,78 @@
 import { Box, IconButton } from '@cypher/front/shared/ui';
 import { Pencil } from 'lucide-react';
-import { useState } from 'react';
-
-// Uppy
+import { useEffect, useMemo, useState } from 'react';
 import Uppy from '@uppy/core';
-import '@uppy/core/dist/style.min.css';
-import '@uppy/dashboard/dist/style.min.css';
-import '@uppy/webcam/dist/style.min.css';
-import '@uppy/image-editor/dist/style.min.css';
 import { DashboardModal } from '@uppy/react';
+import Transloadit from '@uppy/transloadit';
+import ImageEditor from '@uppy/image-editor';
+import { uppyTranslations } from '../translations/uppy';
 
-const bannerPictureUppy = new Uppy({
-  restrictions: {
-    maxNumberOfFiles: 1,
-    maxFileSize: 4000000,
-    allowedFileTypes: ['image/*'],
-  },
-});
+// STYLES
+import '@uppy/core/dist/style.min.css';
+import '@uppy/webcam/dist/style.min.css';
+import '@uppy/dashboard/dist/style.min.css';
+import '@uppy/image-editor/dist/style.min.css';
 
 interface BannerProps {
+  pseudo: string;
   bannerUrl: string | null;
   currentUserIsOnHisProfilePage: boolean;
+  handleUpdateBannerUrl: (url: string) => Promise<boolean>;
 }
 export const Banner = ({
+  pseudo,
   bannerUrl,
   currentUserIsOnHisProfilePage,
+  handleUpdateBannerUrl,
 }: BannerProps) => {
   const [openDashboardModal, setOpenDashboardModal] = useState<boolean>(false);
+
+  const bannerPictureUppy = useMemo(() => {
+    return new Uppy({
+      restrictions: {
+        maxNumberOfFiles: 1,
+        maxFileSize: 10000000,
+        allowedFileTypes: ['image/*'],
+      },
+    })
+      .use(Transloadit, {
+        assemblyOptions: {
+          params: {
+            auth: { key: process.env.NEXT_PUBLIC_TRANSLOADIT_API_KEY },
+            template_id:
+              process.env.NEXT_PUBLIC_TRANSLOADIT_BANNERS_TEMPLATE_ID,
+          },
+        },
+        waitForEncoding: true,
+        locale: { ...uppyTranslations.fr.transloadit },
+      })
+      .use(ImageEditor, {
+        quality: 0.8,
+        cropperOptions: {
+          viewMode: 1,
+          background: false,
+          autoCropArea: 1,
+          responsive: true,
+          croppedCanvasOptions: {},
+        },
+        locale: {
+          ...uppyTranslations.fr.imageEditor,
+        },
+      });
+  }, []);
+
+  useEffect(() => {
+    bannerPictureUppy.setOptions({
+      onBeforeFileAdded: (file) => {
+        file.name = `${pseudo}_${new Date().getTime()}`;
+        return Boolean(file);
+      },
+    });
+
+    bannerPictureUppy.on('transloadit:result', (_, result) => {
+      handleUpdateBannerUrl(result.ssl_url);
+    });
+  }, [pseudo, handleUpdateBannerUrl, bannerPictureUppy]);
 
   return (
     <>
@@ -54,8 +100,8 @@ export const Banner = ({
             sx={{
               borderRadius: '100px',
               position: 'absolute',
-              bottom: '5px',
-              right: '5px',
+              bottom: '10px',
+              right: '10px',
               height: '20px',
               width: '20px',
               display: 'flex',
@@ -75,12 +121,19 @@ export const Banner = ({
       {currentUserIsOnHisProfilePage && (
         <DashboardModal
           uppy={bannerPictureUppy}
-          plugins={['Webcam']}
+          plugins={['ImageEditor']}
           open={openDashboardModal}
           onRequestClose={() => {
             setOpenDashboardModal(false);
           }}
           closeModalOnClickOutside={true}
+          autoOpenFileEditor={true}
+          locale={{
+            strings: {
+              ...uppyTranslations.fr.statusBar.strings,
+              ...uppyTranslations.fr.dashboard.strings,
+            },
+          }}
         />
       )}
     </>
--- libs/front/pages/user-profile/src/lib/components/ProfilePicture.tsx
+++ libs/front/pages/user-profile/src/lib/components/ProfilePicture.tsx
@@ -1,22 +1,19 @@
 'use client';
 import { Box, IconButton, Typography } from '@cypher/front/shared/ui';
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Pencil } from 'lucide-react';
 import Uppy from '@uppy/core';
 import Webcam from '@uppy/webcam';
 import { DashboardModal } from '@uppy/react';
+import Transloadit from '@uppy/transloadit';
+import ImageEditor from '@uppy/image-editor';
+import { uppyTranslations } from '../translations/uppy';
+
+// STYLES
 import '@uppy/core/dist/style.min.css';
 import '@uppy/dashboard/dist/style.min.css';
 import '@uppy/webcam/dist/style.min.css';
 import '@uppy/image-editor/dist/style.min.css';
-
-const profilePictureUppy = new Uppy({
-  restrictions: {
-    maxNumberOfFiles: 1,
-    maxFileSize: 2000000,
-    allowedFileTypes: ['image/*'],
-  },
-}).use(Webcam);
 
 const PROFILE_PICTURE_SIZE = {
   sm: 8.125,
@@ -25,16 +22,68 @@ const PROFILE_PICTURE_SIZE = {
 
 interface ProfilePictureProps {
   pseudo: string;
-  profileUrl?: string | null;
+  profileUrl: string | null;
   currentUserIsOnHisProfilePage: boolean;
+  handleUpdateProfileUrl: (url: string) => Promise<boolean>;
 }
 
 export function ProfilePicture({
   pseudo,
   profileUrl,
   currentUserIsOnHisProfilePage,
+  handleUpdateProfileUrl,
 }: ProfilePictureProps) {
   const [openDashboardModal, setOpenDashboardModal] = useState<boolean>(false);
+
+  const profilePictureUppy = useMemo(() => {
+    return new Uppy({
+      restrictions: {
+        maxNumberOfFiles: 1,
+        maxFileSize: 5000000,
+        allowedFileTypes: ['image/*'],
+      },
+    })
+      .use(Webcam, {
+        modes: ['picture'],
+      })
+      .use(ImageEditor, {
+        quality: 0.8,
+        cropperOptions: {
+          viewMode: 1,
+          background: false,
+          autoCropArea: 1,
+          responsive: true,
+          croppedCanvasOptions: {},
+        },
+        locale: {
+          ...uppyTranslations.fr.imageEditor,
+        },
+      })
+      .use(Transloadit, {
+        assemblyOptions: {
+          params: {
+            auth: { key: process.env.NEXT_PUBLIC_TRANSLOADIT_API_KEY },
+            template_id:
+              process.env.NEXT_PUBLIC_TRANSLOADIT_PROFILES_TEMPLATE_ID,
+          },
+        },
+        waitForEncoding: true,
+        locale: { ...uppyTranslations.fr.transloadit },
+      });
+  }, []);
+
+  useEffect(() => {
+    profilePictureUppy.setOptions({
+      onBeforeFileAdded: (file) => {
+        file.name = `${pseudo}_${new Date().getTime()}`;
+        return Boolean(file);
+      },
+    });
+
+    profilePictureUppy.on('transloadit:result', (_, result) => {
+      handleUpdateProfileUrl(result.ssl_url);
+    });
+  }, [pseudo, handleUpdateProfileUrl, profilePictureUppy]);
 
   return (
     <>
@@ -43,7 +92,7 @@ export function ProfilePicture({
         sx={{
           ...(profileUrl != null
             ? {
-                background: `url("${profileUrl}")`,
+                background: `url(${profileUrl})`,
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
               }
@@ -103,12 +152,19 @@ export function ProfilePicture({
       {currentUserIsOnHisProfilePage && (
         <DashboardModal
           uppy={profilePictureUppy}
-          plugins={['Webcam']}
+          plugins={['Webcam', 'ImageEditor']}
           open={openDashboardModal}
           onRequestClose={() => {
             setOpenDashboardModal(false);
           }}
           closeModalOnClickOutside={true}
+          autoOpenFileEditor={true}
+          locale={{
+            strings: {
+              ...uppyTranslations.fr.statusBar.strings,
+              ...uppyTranslations.fr.dashboard.strings,
+            },
+          }}
         />
       )}
     </>
