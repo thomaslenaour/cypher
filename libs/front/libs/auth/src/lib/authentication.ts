2c28c52a38d83b9f6c760c853e0ad51d96ac761a
Thomas Le Naour <lenaourthomas@gmail.com>
2023-09-22 11:54:28 +0200

    feat(home): add how it works section

--- libs/front/libs/auth/src/lib/authentication.ts
+++ libs/front/libs/auth/src/lib/authentication.ts
@@ -1,6 +1,5 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import FacebookProvider from 'next-auth/providers/facebook';
 import EmailProvider from 'next-auth/providers/email';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import jwt from 'jsonwebtoken';
@@ -49,10 +48,6 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
   ],
   session: {
